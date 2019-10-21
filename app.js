98457708b5c9c6c9ba4765368fa119432834a708
unknown <tsokac2@gmail.com>
2019-10-21 14:50:13 +0100

    final init

---
+++ app.js
@@ -0,0 +1,26 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+
+let posts = [];
+
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+
+let port = process.env.PORT;
+if (port == null || port =="") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started on port 3000 successfully");
+});
